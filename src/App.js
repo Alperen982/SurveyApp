32555bc915be96d96f0b1452f9a0c2bf7741a6a9
Alperen982 <alperen_taflan@hotmail.com>
2025-05-17 01:18:39 +0300

    fixessss

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" />
             <Route path="/Home" element={<Home />} />
             <Route path="/surveys" element={<Surveys />} />
             <Route path="/users/:userId/surveys/:surveyId" element={<SurveyDetails />} /> {/* Anket detay sayfası */}
