58c0075ffced912b747c78ecd977392ace72bef5
Alperen982 <alperen_taflan@hotmail.com>
2025-05-17 00:55:14 +0300

    profile picture changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,16 +8,7 @@ import { AdvancedImage } from '@cloudinary/react';
 import { fill } from "@cloudinary/url-gen/actions/resize";
 
 function Home() {
-  const cld = new Cloudinary({ cloud: { cloudName: 'dq4iweuoy' } });
   
-  // Use this sample image or upload your own via the Media Explorer
-  const img = cld
-        .image('cld-sample-5')
-        .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
-        .quality('auto')
-        .resize(fill().width(300).height(300).gravity(autoGravity())); // Transform the image: auto-crop to square aspect_ratio
-
-  return (<AdvancedImage cldImg={img}/>);
 };
 
 export default Home; 
