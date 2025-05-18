9d46b2a66404a315f4f279d4847da448b1517f7f
Alperen982 <alperen_taflan@hotmail.com>
2025-05-18 17:58:46 +0300

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import React from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import CreateSurvey from './pages/CreateSurvey';
@@ -10,20 +11,81 @@ import Surveys from './pages/Surveys';
 import SurveyDetails from './pages/SurveyDetails';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  if (loading) {
+    return <div className="text-center mt-10">Yükleniyor...</div>;
+  }
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/surveys" element={<Surveys />} />
-            <Route path="/users/:userId/surveys/:surveyId" element={<SurveyDetails />} /> {/* Anket detay sayfası */}
-            <Route path="/create-survey" element={<CreateSurvey />} />
-            <Route path="/survey/:id" element={<SurveyDetail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            {/* Root route: Home if logged in, else Login */}
+            <Route
+              path="/"
+              element={
+                user ? <Home /> : <Navigate to="/login" replace />
+              }
+            />
+
+            {/* Auth-protected routes */}
+            <Route
+              path="/surveys"
+              element={
+                user ? <Surveys /> : <Navigate to="/login" replace />
+              }
+            />
+            <Route
+              path="/users/:userId/surveys/:surveyId"
+              element={
+                user ? <SurveyDetails /> : <Navigate to="/login" replace />
+              }
+            />
+            <Route
+              path="/create-survey"
+              element={
+                user ? <CreateSurvey /> : <Navigate to="/login" replace />
+              }
+            />
+            <Route
+              path="/survey/:id"
+              element={
+                user ? <SurveyDetail /> : <Navigate to="/login" replace />
+              }
+            />
+
+            {/* Public routes */}
+            <Route
+              path="/login"
+              element={
+                !user ? <Login /> : <Navigate to="/" replace />
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                !user ? <Register /> : <Navigate to="/" replace />
+              }
+            />
+
+            {/* Catch-all */}
+            <Route
+              path="*"
+              element={<Navigate to={user ? '/' : '/login'} replace />}
+            />
           </Routes>
         </div>
       </div>
@@ -31,4 +93,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
