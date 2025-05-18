724756a02556edfcdb332a45bb5633f62a75832f
Alperen982 <alperen_taflan@hotmail.com>
2025-05-18 21:42:45 +0300

    tons of FIXESSSSSSSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -170,13 +170,13 @@ function Home() {
         {userData && (
   <div className="text-center mt-2 text-gray-700 w-full">
     <div className="flex justify-center">
-      <p className="bg-gray-100 rounded px-4 py-2 inline-block">
+      <p className="bg-gray-100 rounded px-4 py-2 inline-block text-center w-full">
         <strong>Oluşturulan Etkinlik Sayısı:</strong> {userData.surveyCount ?? 0}
       </p>
     </div>
 
     <div className="flex justify-center mt-2">
-      <p className="bg-gray-100 rounded px-4 py-2 inline-block">
+      <p className="bg-gray-100 rounded px-4 py-2 inline-block text-center w-full">
         <strong>Mail adresi:</strong> {userData.email}
       </p>
     </div>
