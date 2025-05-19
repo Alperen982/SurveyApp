1bc7740e7a9be6506263c3a8b446f75b8d4a548c
Alperen982 <alperen_taflan@hotmail.com>
2025-05-19 16:39:14 +0300

    password reset mail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import ResetPassword from './pages/ResetPassword';
+import ResetPassword from './pages/PasswordReset';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import CreateSurvey from './pages/CreateSurvey';
