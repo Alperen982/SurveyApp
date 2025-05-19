863bc71505c7a3166c54b507a9460e579b36a3b4
Alperen982 <alperen_taflan@hotmail.com>
2025-05-19 14:41:03 +0300

    scroll to top

--- src/pages/CreateSurvey.js
+++ src/pages/CreateSurvey.js
@@ -1,5 +1,4 @@
 import React,  { useState, useEffect }  from 'react';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { db, addDoc, collection, getAuth} from '../Firebase/config';
 import { doc, updateDoc, increment } from 'firebase/firestore';
